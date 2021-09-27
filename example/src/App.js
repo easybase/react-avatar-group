e6e84b57a8a6eafb46a46630194761477ca31039
dilan-dio4 <54545871+dilan-dio4@users.noreply.github.com>
2021-09-27 16:57:55 -0400

    Cleaned up example. Cleaning up README

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,68 @@
 import AvatarGroup from 'react-avatar-group';
-import { names } from './utils';
+import { names, extraColorArray } from './utils';
 import './App.css'
+
+function ExampleGroup({ name, ...props }) {
+  return (
+    <div>
+      <h4>{name}</h4>
+      <AvatarGroup {...props} />
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div style={{ marginLeft: 15 }}>
-      <AvatarGroup avatars={names.slice(0, 1)} />
-      <AvatarGroup avatars={[...names.slice(0, 4), { avatar: "Dilan", backgroundColor: "CCCCCC", fontColor: "000000", tooltip: "F" }]} initialCharacters={1} uppercase size={40} square />
-      <AvatarGroup avatars={names.slice(0, 10)} max={2} uppercase size={60} shadow={2} fontColor="00FF00" displayAllOnHover />
-      <AvatarGroup avatars={names.slice(0, 100)} max={2} uppercase size={60} shadow={2} displayAllOnHover />
-      <AvatarGroup avatars={names.slice(0, 1200)} max={2} uppercase size={60} randomBackgroundColors={["0FFF44", "4411FF"]} shadow={2} />
-      <AvatarGroup avatars={names.slice(0, 100)} initialCharacters={1} max={2} uppercase size={60} square />
+    <div style={{ marginLeft: 15, display: "flex", flexDirection: "column", justifyContent: "space-around", height: "100vh" }}>
+      <ExampleGroup 
+        name="Single Avatar"
+        avatars={names.slice(0, 1)}
+      />
+      <ExampleGroup
+        name="1 Character, uppercase, mixing string and object square avatars"
+        avatars={[...names.slice(0, 4), { avatar: "Dilan", backgroundColor: "CCCCCC", fontColor: "000000", tooltip: "This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. This is a custom tooltip. " }]}
+        initialCharacters={1}
+        uppercase
+        size={40}
+        square
+      />
+      <ExampleGroup 
+        name="Maximum avatars, display all on hover"
+        avatars={names.slice(0, 10)}
+        max={2}
+        uppercase
+        size={60}
+        shadow={2}
+        fontColor="00FF00"
+        displayAllOnHover
+      />
+      <ExampleGroup
+        name="Stress test max, display all on hover"
+        avatars={names.slice(0, 100)}
+        max={2}
+        uppercase
+        size={60}
+        shadow={2}
+        displayAllOnHover
+      />
+      <ExampleGroup
+        name="Stress test max, don't display all on hover, custom random background colors array"
+        avatars={names.slice(0, 1200)}
+        max={25}
+        uppercase
+        size={60}
+        randomBackgroundColors={extraColorArray}
+        shadow={2}
+      />
+      <ExampleGroup
+        name="1 character, tooltip with arrow"
+        avatars={names.slice(0, 100)}
+        initialCharacters={1}
+        max={2}
+        size={60}
+        square
+        tooltipArrow
+      />
     </div>
   );
 }
